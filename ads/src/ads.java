75be260ea2e53b911bdfb8cfe4d66ec19e026482
n-okonek <41804767+n-okonek@users.noreply.github.com>
2021-03-23 20:55:48 -0500

    Updated home page design

--- ads/src/ads.java
+++ ads/src/ads.java
@@ -45,6 +45,7 @@ public class ads extends HttpServlet {
 		
 		PrintWriter out;
 		out = response.getWriter();
+		String title = "Automatic Dispensing Systems";
 		
 		out.print("<!DOCTYPE html>\r\n"
 				+ "<!--[if lt IE 7]>      <html class=\"no-js lt-ie9 lt-ie8 lt-ie7\"> <![endif]-->\r\n"
@@ -59,19 +60,41 @@ public class ads extends HttpServlet {
 				+ "    <meta name=\"description\" content=\"\">\r\n"
 				+ "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n"
 				+ "\r\n	   "
+				+ "<link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.15.3/css/all.css' integrity='sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk' crossorigin='anonymous'>"
 				+ "<!-- JavaScript Bundle with Popper -->\r\n"
 				+ "    <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js\" integrity=\"sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0\" crossorigin=\"anonymous\"></script>\r\n"
 				+ "\r\n    "
 				+ "    <!-- CSS only -->\r\n"
 				+ "    <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl\" crossorigin=\"anonymous\">\r\n"
 				+ "\r\n"
+				+ "    <link href='./css/style.css' rel='stylesheet' />"
 				+ "  </head>\r\n"
 				+ "  <body>\r\n"
 				+ "    <!--[if lt IE 7]>\r\n"
 				+ "      <p class=\"browsehappy\">You are using an <strong>outdated</strong> browser. Please <a href=\"#\">upgrade your browser</a> to improve your experience.</p>\r\n"
 				+ "    <![endif]-->\r\n"
 				+ "    \r\n");
-		out.print("<header>[REPLACE ME WITH A PROPERTY]</header>");
+		out.print("<nav class='navbar navbar-expand-lg navbar-dark bg-dark'>");
+			out.print("<div class='container-fluid'>");
+				out.print("<a class='navbar-brand' href='#'>");
+					out.print("<i class=\"fas fa-prescription\"></i>&nbsp;"+ title);
+				out.print("</a>");
+				out.print("<button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNav\" aria-controls=\"navbarNav\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n"
+						+ "      <span class=\"navbar-toggler-icon\"></span>\r\n"
+						+ "    </button>\r\n"
+						+ "    <div class=\"collapse navbar-collapse\" id=\"navbarNav\">\r\n"
+						+ "      <ul class=\"navbar-nav\">\r\n"
+						+ "        <li class=\"nav-item\">\r\n"
+						+ "          <a class=\"nav-link active\" aria-current=\"page\" href=\"#\">Home</a>\r\n"
+						+ "        </li>\r\n"
+						+ "        <li class=\"nav-item\">\r\n"
+						+ "          <a class=\"nav-link\" href=\"#\">Log In</a>\r\n"
+						+ "        </li>\r\n"
+						+ "      </ul>"
+						+ "    </div>");
+			out.print("</div>");
+		out.print("</nav>");
+		out.print("<div class='bg-img'></div>");
 
 		try {
 			Context initContext = new InitialContext();
